db39a964d19e085f11f0057b1c2e793a672e5da0
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2025-01-31 19:08:40 +0400

    done

---
+++ practise/script.js
@@ -0,0 +1,10 @@
+const button1 = document.getElementById('create-bttn1')
+button1.addEventListener('click',function (event) {
+    event.preventDefault();
+    window.location.href = "create.html";
+})
+const button2 = document.getElementById('create-bttn2')
+button2.addEventListener('click',function (event) {
+    event.preventDefault();
+    window.location.href = "create.html";
+})
